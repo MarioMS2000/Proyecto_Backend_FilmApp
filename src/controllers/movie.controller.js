2f614eb671e3251a224dbe2176fe736c8a6ed339
Karina Rojas <karinacodecompetent@gmail.com>
2026-04-19 21:49:57 +0200

    feat(movie): implement OMDB integration, MongoDB model, service logic and controller structure

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -1,11 +1,31 @@
-const movieController = {
-  index(req, res) {
-    res.render("pages/movies");
-  },
+const {searchMovie,getMovieByIdService} = require("../services/movie.service");
+const Movie = require("../models/mongo/Movie")
 
-  search(req, res) {
-    res.render("pages/search");
-  },
-};
+//USERS buscar peli
+const searchMovies = async (req,res) => {
+  try {
+    const title = req.query.title;
+    const movies = await searchMovie(title);
 
-module.exports = movieController;
+    const response = movies.map((movie) => {
+      return {
+        title: movie.title || movie.Title,
+        year: movie.year || movie.Released,
+        poster: movie.poster || movie.Poster,
+        director: movie.director || movie.Director,
+        genre: movie.genre || movie.Genre,
+        duration: movie.duration || movie.Runtime,
+      };
+    });
+    return res.status(200).json(response);
+
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error buscando películas",
+    });
+  }
+}
+module.exports = {
+  searchMovies,
+  
+}
