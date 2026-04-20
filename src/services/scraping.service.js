960dda0bc40e2c8d79fd43ea5ef25baee308231f
Karina Rojas <karinacodecompetent@gmail.com>
2026-04-20 22:49:23 +0200

    feat: scraping service con puppeteer estructura base

--- src/services/scraping.service.js
+++ src/services/scraping.service.js
@@ -1,7 +1,15 @@
-const scrapingService = {
-  scrape() {
-    return [];
-  },
-};
+const puppeteer = require("puppeteer");
 
-module.exports = scrapingService;
+const getMovieReviews = async (movieTitle) => {
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  try {
+    
+  } catch (error) {
+    await browser.close();
+    console.error("Error en scraping:", error);
+    return [];
+  }
+}
+module.exports = { getMovieReviews };
