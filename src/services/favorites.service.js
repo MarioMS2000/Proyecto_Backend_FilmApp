b55a97f8ef42709ce23ce9656e7251ef13dae702
MarioMS2000 <92580217+MarioMS2000@users.noreply.github.com>
2026-04-23 13:04:40 +0200

    Creación favorites/service/controller/validator/router

--- src/services/favorites.service.js
+++ src/services/favorites.service.js
@@ -1,18 +1,39 @@
 const Favorite = require("../models/sql/Favorite");
 
-const getAllFavoritesMovies = async(userId) =>{
+// GET
+const getAllFavoritesMovies = async (userId) => {
   return Favorite.findAll({
-    where: {user_id: userId},
+    where: { user_id: userId },
     order: [["created_at", "DESC"]],
   });
 }
 
-const addFavoriteMovie = async(userId, favoriteData) =>{
-  
+// POST -> favoriteData = sourceType, sourceMovieId;
+const addFavoriteMovie = async (userId, favoriteData) => {
+  const { sourceType, sourceMovieId } = favoriteData;
+  return Favorite.create({
+    user_id: userId,
+    sourceType,
+    sourceMovieId,
+  });
 }
 
-const removeFavoriteMovie = async(userId, favoriteId) => {
+// DELETE
+const removeFavoriteMovie = async (userId, favoriteId) => {
+  const favorite = await Favorite.findOne({
+    where: {
+      id: favoriteId,
+      user_id: userId,
+    },
+  });
 
+  // si no encontró ningún favorito con ese id y ese user_id
+  if(!favorite){
+    return null;
+  }
+
+  await favorite.destroy();
+  return favorite;
 }
 
 module.exports = {
